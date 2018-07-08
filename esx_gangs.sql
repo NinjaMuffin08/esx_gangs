bcd129ab90003c5f083474bd2cb608438ee4c0c5
NinjaMuffin08 <ralisarichards@gmail.com>
2018-07-08 15:17:23 -0400

    Updated SQL so it could be created

--- esx_gangs.sql
+++ esx_gangs.sql
@@ -1,6 +1,7 @@
-USE `essentialmode`;
+USE `essentialmode`; 
+/* Change essentialmode to your zap server db otherwise you might run into issues */
 
-CREATE TABLE 'gangs' (
+CREATE TABLE IF NOT EXISTS `gangs` (
 
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -9,10 +10,10 @@ CREATE TABLE 'gangs' (
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO gangs (name, label)
+INSERT INTO `gangs` (name, label)
 VALUES ('none', 'Unaffiliated'); 
 
-CREATE TABLE 'gang_grades' (
+CREATE TABLE IF NOT EXISTS `gang_grades` (
 
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gang_name` varchar(255) NOT NULL,
